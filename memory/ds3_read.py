ebe9f54416939571b28044af1bca987e759587c8
Jason Wong <ja2415n@gmail.com>
2026-01-28 23:22:31 -0800

    added boss hp tracking for iudex gundyr and separated helper functions out

---
+++ memory/ds3_read.py
@@ -0,0 +1,40 @@
+from utils import *
+import pymem
+import os
+
+ds3 = pymem.Pymem("DarkSoulsIII.exe")
+module = pymem.process.module_from_name(ds3.process_handle, "DarkSoulsIII.exe")
+
+"""
+These are all pointers. Saved in memory to reduce recomputing.
+"""
+world_chr_man = get_world_chr_man(ds3, module)
+player_stats = follow_chain(ds3, world_chr_man, [0x80, 0x1F90, 0x18])
+player_curr_hp = player_stats + 0xD8
+player_max_hp = player_stats + 0xDC
+player_curr_sp = player_stats + 0xF0
+player_max_sp = player_stats + 0xF4
+
+print("--------- Game Info ----------")
+print(f'Player Current HP: {ds3.read_int(player_curr_hp)}')
+print(f'Player Max HP: {ds3.read_int(player_max_hp)}')
+print(f'Player Current SP: {ds3.read_int(player_curr_sp)}')
+print(f'Player Max SP: {ds3.read_int(player_max_sp)}')
+
+iudex_gundyr = get_entity(ds3, world_chr_man, IUDEX_GUNDYR)
+iudex_curr_hp = iudex_gundyr + 0xD8
+iudex_max_hp = iudex_gundyr + 0xDC
+
+os.system("cls" if os.name == "nt" else "clear")
+print("\033[?25l", end="")
+while True:
+    print("\033[H", end="")
+    
+    print("----- Game Info ------")
+    print(f'Player Current HP: {ds3.read_int(player_curr_hp)}')
+    print(f'Player Max HP: {ds3.read_int(player_max_hp)}')
+    print(f'Player Current SP: {ds3.read_int(player_curr_sp)}')
+    print(f'Player Max SP: {ds3.read_int(player_max_sp)}')
+    print()
+    print(f'Boss Current HP: {ds3.read_int(iudex_curr_hp)}')
+    print(f'Boss Max HP: {ds3.read_int(iudex_max_hp)}')
---
+++ memory/utils.py
@@ -0,0 +1,62 @@
+import pymem
+
+
+IUDEX_GUNDYR = 1037
+
+
+def follow_chain(ds3, addr, offsets, debug=True):
+    ptr = addr
+    for offset in offsets:
+        try:
+            ptr = ds3.read_longlong(ptr + offset)
+
+            if debug:
+                print(hex(ptr))
+
+        except Exception as e:
+            print("Error handling memory offsets. Most likely harmless but a chain did fail.")
+
+            if debug:
+                print(e)
+
+    return ptr
+
+
+"""
+Scans DS3 memory using AOB (Array of Bytes) to find WorldChrMan.
+WorldChrMan is the root pointer in the pointer tree for almost all entities.
+"""
+def get_world_chr_man(ds3, module):
+    pattern = b"\x48\x8B\x1D...\x04\x48\x8B\xF9\x48\x85\xDB..\x8B\x11\x85\xD2..\x8D"
+
+    instr = pymem.pattern.pattern_scan_module(
+        ds3.process_handle,
+        module,
+        pattern,
+        return_multiple=False
+    )
+    offset = ds3.read_int(instr + 3)
+    world_chr_man_addr = instr + 7 + offset
+
+    return ds3.read_longlong(world_chr_man_addr)
+
+
+"""
+Currently checks using entity Max HP. 
+Ideally would use NPCParam (ID), but can't figure out the offsets.
+"""
+def get_entity(ds3, world_chr_man, entity_max_hp):
+    chr_num = ds3.read_int(ds3.read_longlong(world_chr_man + 0x1D0))
+    chr_set = follow_chain(ds3, world_chr_man, [0x1D0, 0x8])
+    entity = None
+    
+    print(chr_num)
+    for i in range(chr_num):
+        sprj_chr_data_module = follow_chain(ds3, chr_set, [i * 0x38, 0x1F90, 0x18])
+        if ds3.read_int(sprj_chr_data_module + 0xDC) == entity_max_hp:
+            entity = sprj_chr_data_module
+    
+    if entity is None: 
+        raise ValueError(f'Entity with identifier [{entity_max_hp}] could not be found.')
+
+    return entity
