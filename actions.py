9fd51115b2abaada3060bf91c261f942d5462b82
leahs4 <leahs4@uci.edu>
2026-01-31 02:25:05 -0800

    Added simple skeleton ds3Env gym class

--- actions.py
+++ actions.py
@@ -2,40 +2,6 @@ import subprocess
 from pathlib import Path
 import pydirectinput as pdi
 import time
-#this prob irrelevant to project, but might help if we use pydirectinput
-
-(leah, jason, dustin) = (True, False, False) #CHANGE! 
-
-def enter_game():
-    '''this actually launches the bat file for you and clicks any button to enter the game menu... 
-            just an experiment we can add to if we want to use this logic'''
-    leah_path = r"C:\Users\leahs\Downloads\Boss Arena (Sandbox Mode)-1854-Sandbox-2-3-1758292375\Dark Souls 3 Boss Arena (Sandbox 2.3)\launch.bat"
-    jason_path = r""
-    dustin_path = r""
-
-    user_dict = {leah_path : leah, jason_path : jason, dustin_path : dustin}
-    use_path = ""
-    for user in user_dict:
-        if(user_dict[user]):
-            use_path = user
-
-    launchbat_path = Path(use_path)
-    subprocess.Popen(["cmd", "/c", str(launchbat_path)], cwd=str(launchbat_path.parent))
-                    #run in cmd, then /c tells it to exit cmd after we launch path. 
-                        #cwd => sandbox 2.3 folder
-    time.sleep(15)
-    pdi.click(960, 540) 
-    time.sleep(0.2)
-
-    print("trying to right click")
-
-    pdi.mouseDown(button="right")
-    time.sleep(0.05)
-    pdi.mouseUp(button="right")
-    time.sleep(3)
-
-    print("stopped trying")
-#enter_game()
 
 def right_hand_light_attack():
     pdi.click()
@@ -90,6 +56,40 @@ def run_left(sec):
     pdi.keyUp("a")
     pdi.keyUp(" ")
 
-time.sleep(2)
+def heal():
+    pdi.press("r")
 
-run_forward(5)
+#this prob irrelevant to project, but might help if we use pydirectinput
+
+(leah, jason, dustin) = (True, False, False) #CHANGE! 
+
+def enter_game():
+    '''this actually launches the bat file for you and clicks any button to enter the game menu... 
+            just an experiment we can add to if we want to use this logic'''
+    leah_path = r"C:\Users\leahs\Downloads\Boss Arena (Sandbox Mode)-1854-Sandbox-2-3-1758292375\Dark Souls 3 Boss Arena (Sandbox 2.3)\launch.bat"
+    jason_path = r""
+    dustin_path = r""
+
+    user_dict = {leah_path : leah, jason_path : jason, dustin_path : dustin}
+    use_path = ""
+    for user in user_dict:
+        if(user_dict[user]):
+            use_path = user
+
+    launchbat_path = Path(use_path)
+    subprocess.Popen(["cmd", "/c", str(launchbat_path)], cwd=str(launchbat_path.parent))
+                    #run in cmd, then /c tells it to exit cmd after we launch path. 
+                        #cwd => sandbox 2.3 folder
+    time.sleep(15)
+    pdi.click(960, 540) 
+    time.sleep(0.2)
+
+    print("trying to right click")
+
+    pdi.mouseDown(button="right")
+    time.sleep(0.05)
+    pdi.mouseUp(button="right")
+    time.sleep(3)
+
+    print("stopped trying")
+#enter_game()
