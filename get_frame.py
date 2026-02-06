59132b660d28a705449fbecb0376353f208da636
Jason Wong <ja2415n@gmail.com>
2026-02-05 16:16:36 -0800

    change image processing to 128x128 grayscale

--- get_frame.py
+++ get_frame.py
@@ -1,18 +1,18 @@
 import mss
 import numpy as np
 import cv2
-import time
 import ctypes
 ctypes.windll.user32.SetProcessDPIAware()
 import win32gui
+import time
 
 def get_one_frame_fullscreen():
     '''takes 1 ss of the game in fullscreen mode. must change to the right monitor & the game to capture the screen'''
-    scale_factor = 0.5
     with mss.mss() as sct:
         monitor = sct.monitors[2]
         frame = np.array(sct.grab(monitor))[:, :, :3]
-    frame = cv2.resize(frame, (400, 400), fx=scale_factor, fy=scale_factor, interpolation=cv2.INTER_AREA) #can change size 400x400, if needed
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    frame = cv2.resize(frame, (128, 128), interpolation=cv2.INTER_AREA)
     cv2.imwrite("ds3_frame.png", frame)
     print("Saved ds3_frame.png")
 
@@ -45,14 +45,18 @@ def get_one_frame():
             "height": h
         }
         frame = np.array(sct.grab(monitor))[:, :, :3]
-    frame = cv2.resize(frame, (400, 400), interpolation=cv2.INTER_AREA) #can change size 400x400, if needed
+
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    frame = cv2.resize(frame, (128, 128), interpolation=cv2.INTER_AREA) 
+    frame = frame.reshape(128, 128, 1)
+
     return frame
 
-# Uncomment to test frame capture
-# while True:
-#     frame = get_one_frame()
-#     if frame is not None:
-#         cv2.imwrite("ds3_frame.png", frame)
-#         print("Saved ds3_frame.png")
-#     time.sleep(0.5)
+if __name__ == "__main__":
+    while True:
+        frame = get_one_frame()
+        if frame is not None:
+            cv2.imwrite("ds3_frame.png", frame)
+            print("Saved ds3_frame.png")
+        time.sleep(0.5)
 
